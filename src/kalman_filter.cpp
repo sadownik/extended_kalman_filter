c402aba81079703ca6d201b75495e5045c370190
sadownik <dnl.sadowski@gmail.com>
2020-02-12 10:16:44 +0000

    First change

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,16 +26,58 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  Tools tools;
+  MatrixXd Hj_ = tools.CalculateJacobian(x_);
+  cout << "Hj_: " << Hj_ << endl;
+  VectorXd hx(3);
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+  float rho = sqrt(px*px + py*py);
+  float phi = atan2(py, px);
+  float rho_dot = (px*vx + py*vy)/rho;
+  hx << rho, phi, rho_dot;
+  cout << "hx: " << hx << endl;
+  VectorXd y = z - hx;
+  cout << "y: " << y << endl;
+  cout << "----------------" << endl;
+  MatrixXd Hj_t = Hj_.transpose();
+  MatrixXd S = Hj_ * P_ * Hj_t + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Hj_t;
+  MatrixXd K = PHt * Si;
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * Hj_) * P_;
 }
